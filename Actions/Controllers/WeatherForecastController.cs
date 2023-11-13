042281f38248381cf01a3705383bcb611c9d5b02
Robertas Riedkus <robertas.riedkus@gmail.com>
2023-11-13 11:46:15 +0200

    Update WeatherForecastController.cs

--- Actions/Controllers/WeatherForecastController.cs
+++ Actions/Controllers/WeatherForecastController.cs
@@ -8,7 +8,7 @@ namespace Actions.Controllers
 	{
 		private static readonly string[] Summaries = new[]
 		{
-		"Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+		"Freezingg", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
 	};
 
 		private readonly ILogger<WeatherForecastController> _logger;
